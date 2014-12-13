3ab4dd3e05abd7a540a2c2f06764b97fc826ae73
Wilhelm Hedman <w.hedman@gmail.com>
2014-12-13 10:21:49 +0100

    Added more tests to the Reservation procedure

--- hotel/src/test/groovy/hotelService/impl/ReservationManagerImplTest.groovy
+++ hotel/src/test/groovy/hotelService/impl/ReservationManagerImplTest.groovy
@@ -1,15 +1,17 @@
 package hotelService.impl
 
-import hotelCore.Reservation
 import hotelCore.Room
 import hotelCore.RoomType
 import se.chalmers.mdsd1415.group11.HotelBaseSpecification
+import spock.lang.Shared
 
 /**
  * Created by wilhelm 2014-12-12.
  */
 class ReservationManagerImplTest extends HotelBaseSpecification {
+    @Shared
     Date today = new Date()
+    @Shared
     Date tomorrow = new Date()+1
     RoomType a
     RoomType b
@@ -49,6 +51,21 @@ class ReservationManagerImplTest extends HotelBaseSpecification {
         bookingManager.allBookings.size() == 1
     }
 
+    def "make two non-overlapping reservations for the same room"() {
+        when:
+        def booking1 = bookingManager.createBooking()
+        def res1 = reservationManager.createReservation(booking1, today, tomorrow, one, a)
+        def booking2 = bookingManager.createBooking()
+        def res2 = reservationManager.createReservation(booking2, tomorrow+1, tomorrow+3, one, a)
+
+        then:
+        booking1.getReservation().size() == 1
+        booking1.getReservation().get(0) == res1
+        booking2.getReservation().size() == 1
+        booking2.getReservation().get(0) == res2
+        bookingManager.allBookings.size() == 2
+    }
+
     def "make the same reservation twice"() {
         when:
         def booking = bookingManager.createBooking()
@@ -73,13 +90,25 @@ class ReservationManagerImplTest extends HotelBaseSpecification {
     }
 
     def "make a reservation that overlaps with another reservation"() {
+        when:
+        def booking = bookingManager.createBooking()
+        reservationManager.createReservation(booking, baseStart, baseEnd, one, a)
+        reservationManager.createReservation(booking, secondStart, secondEnd, one, a)
 
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        baseStart << [today, today, tomorrow]
+        baseEnd << [tomorrow, tomorrow+1, tomorrow+1]
+        secondStart << [today, tomorrow, today]
+        secondEnd << [tomorrow, tomorrow+1, tomorrow]
     }
 
     def "silly dates thow"() {
         when:
         def booking = bookingManager.createBooking()
-        def first = reservationManager.createReservation(booking, tomorrow, today, one, a)
+        reservationManager.createReservation(booking, tomorrow, today, one, a)
 
         then:
         thrown(IllegalArgumentException)
