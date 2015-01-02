991a4869316e35904ae9ea9c4de1e49d5e60bb90
Wilhelm Hedman <w.hedman@gmail.com>
2015-01-02 09:06:05 +0100

    Renamed variables

--- hotel/src/test/groovy/se/chalmers/mdsd1415/group11/usecase/ReserveARoomTest.groovy
+++ hotel/src/test/groovy/se/chalmers/mdsd1415/group11/usecase/ReserveARoomTest.groovy
@@ -64,25 +64,25 @@ class ReserveARoomTest extends HotelBaseSpecification {
 
     def "conference room"() {
         setup:
-        RoomType roomType2 = roomTypeManager.createConferenceRoom("room 1", 50, 1000)
-        Room room2 = roomManager.createRoom(4, roomType2)
-        Booking booking2 = bookingManager.createBooking()
-        def searchCriteria2 = new EArrayList<RoomType>()
-        searchCriteria2.add(roomType2)
-        def availableRooms = roomManager.getAvailableRooms(today, tomorrow, searchCriteria2)
-        bookingManager.allBookings.add(booking2)
+        RoomType conferenceRoom = roomTypeManager.createConferenceRoom("Conference Room 1", 50, 1000)
+        Room conferenceRoom1 = roomManager.createRoom(101, conferenceRoom)
+        Booking booking = bookingManager.createBooking()
+        def conferenceSearch = new EArrayList<RoomType>()
+        conferenceSearch.add(conferenceRoom)
+        def availableRooms = roomManager.getAvailableRooms(today, tomorrow, conferenceSearch)
+        bookingManager.allBookings.add(booking)
 
         when:
-        Reservation reservation2 = reservationManager.createReservation(booking2, today, tomorrow, availableRooms.get(0), roomType2)
+        Reservation reservation = reservationManager.createReservation(booking, today, tomorrow, availableRooms.get(0), conferenceRoom)
 
         then:
-        reservation2.getStartDay() == today
-        reservation2.getEndDay()  == tomorrow
-        reservation2.getRoom() == room2
-        reservation2.getRoom().getRoomType() == roomType2
-        booking2.getReservations().size() == 1
-        booking2.getReservations().get(0) == reservation2
-        booking2.getBill().getGrandTotal() == 1000
+        reservation.getStartDay() == today
+        reservation.getEndDay()  == tomorrow
+        reservation.getRoom() == conferenceRoom1
+        reservation.getRoom().getRoomType() == conferenceRoom
+        booking.getReservations().size() == 1
+        booking.getReservations().get(0) == reservation
+        booking.getBill().getGrandTotal() == 1000
     }
 
     def "no room with desired room type"() {
