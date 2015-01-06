8a00992f555e29ba86bee5555bf2fa4ef0c73aee
Wilhelm Hedman <hedmanw@student.chalmers.se>
2015-01-06 14:27:53 +0100

    Corrected Booking manager banking issues

--- hotel/src/test/groovy/se/chalmers/mdsd1415/group11/usecase/CancelABookingTest.groovy
+++ hotel/src/test/groovy/se/chalmers/mdsd1415/group11/usecase/CancelABookingTest.groovy
@@ -1,8 +1,5 @@
 package se.chalmers.mdsd1415.group11.usecase
 
-import bankingService.BankingSingleton
-import bankingService.CustomerProvides
-import hotelCore.Booking
 import hotelCore.Customer
 import hotelCore.Room
 import hotelCore.RoomType
@@ -12,7 +9,6 @@ import se.chalmers.mdsd1415.group11.HotelBaseSpecification
  * Created by Sofia Edström on 06/01/15.
  */
 class CancelABookingTest extends HotelBaseSpecification{
-    def bank = Mock(CustomerProvides)
     RoomType roomType
     Customer customer
     Room room
@@ -20,13 +16,10 @@ class CancelABookingTest extends HotelBaseSpecification{
 
 
     def setup(){
-        BankingSingleton.instance.CUSTOMER_PROVIDES = bank
-
         roomType = roomTypeManager.createSleepRoom("double room", 1000, 2)
         room = roomManager.createRoom(1, roomType)
         customer = customerManager.createCustomer("1", "Robert C. Martin")
         customerManager.setPaymentDetailsForCustomer(customer, "Robert Cecil", "Martin", "123", "123", 1, 2016)
-        bank.isCreditCardValid("123", "123", 1, 2016, "Robert Cecil", "Martin") >> true
 
     }
 
